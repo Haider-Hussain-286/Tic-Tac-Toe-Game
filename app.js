c79745c2babe7a4c7d68c04652c016c1f719d7aa
Haider-Hussain-286 <haiderhussain.2372@gmail.com>
2025-12-22 19:07:26 +0500

    Refactor game logic and add game modes
    Refactor game logic for improved readability and maintainability. Added functionality for player vs player and player vs computer modes.

--- app.js
+++ app.js
@@ -8,253 +8,155 @@ let playBtn = document.querySelector("#play-btn");
 let playerVP = document.querySelector("#pvp");
 let homeScreen = document.querySelector(".home-screen"); 
 let backMenu = document.querySelector("#back-btn");
+let turnO = true;
+let isPvC = true;
+let gameActive = true;
+
 
 
 container.classList.add("hide");
 resetBtn.classList.add("hide");
 backMenu.classList.add("hide");
 
-// Player vs Computer
+//Some conditions to check winner
+//Pattern
 
-const playerVComputer = () => {
+const winPatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6],
+];
 
-    let playerTurn = true; // true = O (User), false = X (Computer)
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
 
-    const winPatterns = [
-        [0, 1, 2],
-        [0, 3, 6],
-        [0, 4, 8],
-        [1, 4, 7],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8],
-        [2, 4, 6],
-    ];
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("Winner Is ", pos1Val);
+                showWinner(pos1Val);
+                return true;
+            }
+        }
+    }
 
-    const resetGame = () => {
-        playerTurn = true;
-        enableBoxes();
-        msgContainer.classList.add("hide");
-    };
+    //In case of Game has been tied
+    let allFilled = true;
 
-    const backBtn = () => {
+    boxes.forEach((box) => {
+        if (box.innerText === ""){
+           allFilled = false;
+        }   
+    });
+
+    if(allFilled){
+       msg.innerText = "Game Tied, Try Again";
+       msgContainer.classList.remove("hide");
+       disabledBoxes();
+    }
+};
+
+//enable, disable
+const disabledBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+//Reset & Back New Game Buttons
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    gameActive = true; 
+};
+
+const backBtn = () => {
     container.classList.add("hide");
+    msgContainer.classList.add("hide");
     resetBtn.classList.add("hide");
     backMenu.classList.add("hide");
     homeScreen.classList.remove("hide");
-    };
+};
+ 
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+backMenu.addEventListener("click", backBtn);
 
+const startGame = (mode) => {
+    resetGame();
+    gameActive = true; // Essential to re-enable clicking
+    isPvC = (mode === "pvc");
 
-    // User + Computer Moves
-    boxes.forEach((box, index) => {
-        box.addEventListener("click", () => {
+    homeScreen.classList.add("hide");
+    container.classList.remove("hide");
+    resetBtn.classList.remove("hide");
+    backMenu.classList.remove("hide");
+};
 
-            // User move
-            if (playerTurn && box.innerText === "") {
-                box.innerText = "O";
-                box.disabled = true;
-                playerTurn = false;
-                checkWinner();
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        // If game is over or box is full, do nothing
+        if (!gameActive || box.innerText !== "") return;
 
-                // Computer move only if game not ended
+        if (isPvC) {
+            // --- COMPUTER MODE LOGIC ---
+            box.innerText = "O";
+            box.disabled = true;
+            turnO = false;
+            
+            if (!checkWinner()) { // Only computer moves if no one won
                 setTimeout(() => {
                     computerMove();
                 }, 300);
             }
-        });
-    });
-
-    // computer chooses empty box randomly
-    const computerMove = () => {
-
-        if (playerTurn) return; // skip if player's turn
-
-        const emptyBoxes = [];
-
-        boxes.forEach((box, idx) => {
-            if (box.innerText === "") emptyBoxes.push(idx);
-        });
-
-        if (emptyBoxes.length === 0) return;
-
-        const randomIndex = emptyBoxes[Math.floor(Math.random() * emptyBoxes.length)];
-
-        boxes[randomIndex].innerText = "X";
-        boxes[randomIndex].disabled = true;
-
-        playerTurn = true;
-        checkWinner();
-    };
-
-    const disableBoxes = () => {
-        for (let box of boxes) {
+        } else {
+            // --- PLAYER VS PLAYER LOGIC ---
+            box.innerText = turnO ? "O" : "X";
+            turnO = !turnO;
             box.disabled = true;
+            checkWinner();
         }
-    };
-
-    const enableBoxes = () => {
-        for (let box of boxes) {
-            box.disabled = false;
-            box.innerText = "";
-        }
-    };
-
-    const showWinner = (winner) => {
-        msg.innerText = `Congratulations, Winner is ${winner}`;
-        msgContainer.classList.remove("hide");
-        disableBoxes();
-    };
-
-    const checkWinner = () => {
-
-        for (let pattern of winPatterns) {
-            let pos1 = boxes[pattern[0]].innerText;
-            let pos2 = boxes[pattern[1]].innerText;
-            let pos3 = boxes[pattern[2]].innerText;
-
-            if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
-                if (pos1 === pos2 && pos2 === pos3) {
-                    showWinner(pos1);
-                    return;
-                }
-            }
-        }
-
-        // Tie
-        let allFilled = true;
-        boxes.forEach((box) => {
-            if (box.innerText === "") allFilled = false;
-        });
-
-        if (allFilled) {
-            msg.innerText = "Game Tied!, Try Again";
-            msgContainer.classList.remove("hide");
-            disableBoxes();
-        }
-    };
-
-    newGameBtn.addEventListener("click", resetGame);
-    resetBtn.addEventListener("click", resetGame);
-    backMenu.addEventListener("click",backBtn);
-};
-
-
-const play1 = () => {
-    container.classList.remove("hide");
-    resetBtn.classList.remove("hide");
-    backMenu.classList.remove("hide");
-    homeScreen.classList.add("hide");
-    playerVComputer();
-};
-
-playBtn.addEventListener("click",play1);
-
-//Player vs Player   
-
-const playerVPlayer = () => {
-let turnO = true;
-
-    const winPatterns = [
-       [0, 1, 2],
-       [0, 3, 6],
-       [0, 4, 8],
-       [1, 4, 7],
-       [2, 5, 8],
-       [3, 4, 5],
-       [6, 7, 8],
-       [2, 4, 6,]
-    ];
-
-    const resetGame = () => {
-       turnO = true;
-       enabledBoxes();
-       msgContainer.classList.add("hide");
-    };
-
-    const backBtn = () => {
-       container.classList.add("hide");
-       resetBtn.classList.add("hide");
-       backMenu.classList.add("hide");
-       homeScreen.classList.remove("hide");
-    };
-
-
-    boxes.forEach((box) => {
-        box.addEventListener("click",() =>{
-          console.log("button was clicked");
-          if (turnO) {
-             box.innerText = "O";
-             turnO = false;
-            } else{
-             box.innerText ="X";
-             turnO = true;
-            }
-           box.disabled = true; 
-
-           checkWinner();
-        });
     });
+});
 
-    const disabledBoxes = () => {
-        for(let box of boxes) {
-          box.disabled = true;
-        }
-    }
+const computerMove = () => {
+    if (!gameActive || turnO) return; 
 
-    const enabledBoxes = () => {
-        for(let box of boxes) {
-          box.disabled = false;
-          box.innerText = "";
-        }
-    }
+    const emptyBoxes = [...boxes].filter(box => box.innerText === "");
+    if (emptyBoxes.length === 0) return;
 
-    const showWinner = (winner) => {
-        msg.innerText = `Congratulations, Winner is ${winner}`;
-        msgContainer.classList.remove("hide");
-        disabledBoxes();
-    }
+    const randomIndex = Math.floor(Math.random() * emptyBoxes.length);
+    let box = emptyBoxes[randomIndex];
 
-    const checkWinner = () => {
-        for(let pattern of winPatterns){
-           let pos1Val = boxes[pattern[0]].innerText;
-           let pos2Val = boxes[pattern[1]].innerText;
-           let pos3Val = boxes[pattern[2]].innerText;
-
-           if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
-               if (pos1Val === pos2Val && pos2Val === pos3Val) {
-                  console.log("Winner Is ", pos1Val);
-                  showWinner(pos1Val);
-                }
-            }
-        }
-
-        let allFilled = true;
-          boxes.forEach((box) => {
-            if (box.innerText === ""){
-                allFilled = false;
-            } 
-        });
-
-        if(allFilled){
-            msg.innerText = "Game Tied, Try Again";
-            msgContainer.classList.remove("hide");
-            disabledBoxes();
-        }
-    }
-
-    newGameBtn.addEventListener("click", resetGame);
-    resetBtn.addEventListener("click", resetGame);
-    backMenu.addEventListener("click",backBtn);
+    box.innerText = "X";
+    box.disabled = true;
+    turnO = true;
+    checkWinner();
 };
 
-const play2 = () => {
-    homeScreen.classList.add("hide");
-    container.classList.remove("hide");
-    resetBtn.classList.remove("hide");
-    backMenu.classList.remove("hide");
-    playerVPlayer();
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disabledBoxes();
+    gameActive = false; // 👈 This kills the "Memory" of the current game
 };
 
-playerVP.addEventListener("click", play2);
-
-
+// Now your button listeners are very simple:
+playBtn.addEventListener("click", () => startGame("pvc"));
+playerVP.addEventListener("click", () => startGame("pvp"));
+//22-12-2025 built version 2.1
